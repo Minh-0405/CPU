15eaa0289a33f71ac8bff4be40b63f704b91ec14
Minh-0405 <minh.du040506@hcmut.edu.vn>
2025-09-21 22:49:25 +0700

    Update ALU.sv

--- Project/ALU.sv
+++ Project/ALU.sv
@@ -4,13 +4,9 @@ module ALU (
     output logic [7:0] rd,
     output logic is_zero
 );
-    assign is_zero = ( rs1 == 0);
+    assign is_zero = !(|rs1);
     logic [7:0] lut [0:7];
     always_comb begin
-        if (is_zero) begin
-            rd = 8'b0;
-        end
-        else begin 
             lut[2] = rs1 + rs2; // 010
             lut[3] = rs1 & rs2; //011
             lut[4] = rs1 ^ rs2; //100
