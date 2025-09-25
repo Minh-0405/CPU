2c480f2276eb0fc4af5f06b2b9749a1389ecd46b
Minh-0405 <minh.du040506@hcmut.edu.vn>
2025-09-25 10:28:05 +0700

    Update CONTROL.sv

--- Project/CONTROL.sv
+++ Project/CONTROL.sv
@@ -34,7 +34,7 @@ end
     
 
     
-    logic ACC_LOAD, ACC_MEM, STO, HALT, JMP, SKZ ;
+    logic ACC_LOAD, ACC_MEM, STO, HALT, JMP, SKZ, ADDR_IN ;
     always_comb
     begin
         ACC_LOAD = (opcode == 2 | opcode == 3 | opcode == 4 | opcode == 5) ;
@@ -60,20 +60,20 @@ end
     end else begin
         case (state)
             s1: begin // FETCH
-                pc_load = 0;
+					 pc_load = JMP | (SKZ & is_zero);
                 pc_en = 1; halt = 0; jmp = JMP;
                 accumulator_control = 0; accumulator_load = 0;
-                memIns_en = 0; memDa_en = 0; memDa_we = 0;
+                memIns_en = 1; memDa_en = 0; memDa_we = 0;
             end
             s2: begin // DECODE
-                pc_load = JMP | (SKZ & is_zero); pc_en = 0; halt = 0; jmp = JMP;
+                pc_load = 0; pc_en = 0; halt = 0; jmp = JMP;
                 accumulator_control = 0; accumulator_load = 0;
-                memIns_en = 1; memDa_en = 0; memDa_we = 0;
+                memIns_en = 0; memDa_en = 1; memDa_we = 0;
             end
             s3: begin // EXECUTE
                 pc_load = 0; pc_en = 0; halt = HALT; jmp = JMP;
                 accumulator_control = 0; accumulator_load = 0;
-                memIns_en = 0; memDa_en = 1; memDa_we = 0;
+                memIns_en = 0; memDa_en = 0; memDa_we = 0;
             end
             s4: begin // WRITEBACK
                 pc_load = 0; pc_en = 0; halt = HALT; jmp = JMP;
