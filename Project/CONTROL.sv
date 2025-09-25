ac6ea2f422a7d6557b705d50f3a8201d9db55c14
Minh-0405 <minh.du040506@hcmut.edu.vn>
2025-09-25 13:41:33 +0700

    Update CONTROL.sv

--- Project/CONTROL.sv
+++ Project/CONTROL.sv
@@ -60,7 +60,7 @@ end
     end else begin
         case (state)
             s1: begin // FETCH
-					 pc_load = JMP | (SKZ & is_zero);
+					 pc_load = (opcode === 3'bxxx) ? 0 : (JMP | (SKZ & is_zero));
                 pc_en = 1; halt = 0; jmp = JMP;
                 accumulator_control = 0; accumulator_load = 0;
                 memIns_en = 1; memDa_en = 0; memDa_we = 0;
@@ -76,6 +76,7 @@ end
                 memIns_en = 0; memDa_en = 0; memDa_we = 0;
             end
             s4: begin // WRITEBACK
+                pc_load = 0; pc_en = 0; halt = HALT; jmp = JMP;
                 pc_load = 0; pc_en = 0; halt = HALT; jmp = JMP;
                 accumulator_control = ACC_MEM;
                 accumulator_load = ACC_LOAD;
